5e1d49fc5b0f83105769da201a6e1fc125d2dd02
MANI <chandramanigupta2003@gmail.com>
2024-09-27 01:25:02 +0530

    page.tsx

--- chatbot/src/app/[...url]/page.tsx
+++ chatbot/src/app/[...url]/page.tsx
@@ -3,6 +3,15 @@ interface PageProps {
 }
 
 const Page = ({ params }: { params: PageProps }) => {
+    const reconstructedUrl = reconstructUrl(url: params.url as string[]);
   console.log(params);
+  await ragChat.context.add({
+    type: "html",
+    source: 
+  })
+
+
+return 
+
 };
 export default Page;
